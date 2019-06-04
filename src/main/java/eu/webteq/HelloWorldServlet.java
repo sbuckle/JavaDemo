4f4183f03b85c8a0015fc7560e9fa29870ca27ea
Simon Buckle <simon.buckle@gmail.com>
2019-06-04 16:21:46 +0100

    Initial import

---
+++ src/main/java/eu/webteq/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package eu.webteq;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorldServlet extends HttpServlet {
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws IOException, ServletException
+	{
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+
+		try {
+			out.print("Hello World!");
+		} finally {
+			out.close();
+		}
+	}
+}
---
+++ src/test/java/eu/webteq/HelloWorldServletTest.java
@@ -0,0 +1,45 @@
+package eu.webteq;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+ 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+ 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class HelloWorldServletTest {
+
+	@Mock
+	HttpServletRequest request;
+
+	@Mock
+	HttpServletResponse response;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void testGreeting() throws IOException, ServletException {
+		// when(request.getParameter("msg")).thenReturn("hello");
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+
+		when(response.getWriter()).thenReturn(pw);
+
+		HelloWorldServlet servlet = new HelloWorldServlet();
+		servlet.doGet(request, response);
+		String result = sw.getBuffer().toString().trim();
+		assertEquals(result, new String("Hello World!"));
+	}
+}
